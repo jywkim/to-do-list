5f281c914f9bd8fa549285e53dd20553d7c01cd5
jywkim <jkim589@gmail.com>
2018-02-08 15:57:22 -0500

    create Input component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
+import Input from './components/Input';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <div className="to-do-wrapper">
           <Header />
+          <Input />
         </div>
       </div>
     );
